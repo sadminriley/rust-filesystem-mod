f73d32545c1b8882096ed2403d0ac2b6a0b4f79e
Riley Louvier <rileylouvier@MacBook-Pro.local>
2024-05-30 01:17:11 -0600

    Run cargo fmt so we can have some justice in the world

--- src/filesystem_module/filesystem.rs
+++ src/filesystem_module/filesystem.rs
@@ -1,19 +1,20 @@
-use std::io::Write;
 use std::fs;
-
+use std::io::Write;
 
 // public module fn
 pub fn create_fs(files: &[(String, String)]) -> assert_fs::TempDir {
-
     // Create tmp directory
     let temp_dir = assert_fs::TempDir::new().expect("Error creating temporary directory!");
 
     for (name, content) in files {
         let path = temp_dir.path().join(name);
-        fs::create_dir_all(path.parent().expect("Invalid parent directory name or location!"))
-            .expect("Error creating parent directories!");
+        fs::create_dir_all(
+            path.parent()
+                .expect("Invalid parent directory name or location!"),
+        )
+        .expect("Error creating parent directories!");
 
-    // Create and write file contents
+        // Create and write file contents
         let mut file = fs::File::create(&path).expect("Error creating file!");
         file.write_all(content.as_bytes())
             .expect("Error! Could not write to file");
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,16 @@ mod filesystem_module {
     pub mod filesystem;
 }
 
-use filesystem_module::filesystem;
-use std::io::Write;
-use std::fs;
 use assert_fs::prelude::*;
+use filesystem_module::filesystem;
+use std::fs;
+use std::io::Write;
 
 fn main() {
     let files = vec![("file.txt".to_string(), "content".to_string())];
     let temp_dir = filesystem::create_fs(&files);
 
-    temp_dir
-        .child("file.txt")
-        .assert("content");
+    temp_dir.child("file.txt").assert("content");
 
     println!("File system created!");
 
@@ -23,21 +21,23 @@ fn main() {
     // Write content to the file
     let mut file = std::fs::File::create(&file_path).expect("Failed to create file!");
 
-    file.write_all(b"Goodbye, Python!").expect("Failed to write to file!");
-
+    file.write_all(b"Goodbye, Python!")
+        .expect("Failed to write to file!");
 
     // Check if file exists
     assert!(file_path.exists());
-    assert_eq!(std::fs::read_to_string(&file_path).unwrap(), "Goodbye, Python!");
-    
+    assert_eq!(
+        std::fs::read_to_string(&file_path).unwrap(),
+        "Goodbye, Python!"
+    );
+
     // Print content to stdout
     let file_contents = fs::read_to_string(&file_path).expect("Failed to read file");
     println!("Loaded File contents: {}", file_contents);
-} 
-
+}
 
 fn delete_path(path: &std::path::Path) -> std::io::Result<()> {
-    /* 
+    /*
     Possible usage case(???)
     delete_path(&directory_path)
     let directory_path = std::path::Path::new("/foobar/dir");
@@ -53,4 +53,4 @@ fn delete_path(path: &std::path::Path) -> std::io::Result<()> {
         println!("Path {:?} Not found!", path)
     }
     Ok(())
-    }
+}
